b46ee40716bd56a437529b7f360f44b2563ece25
Abdrahman Oladimeji <abdrahmanoladimeji02@gmail.com>
2024-09-18 19:55:44 +0100

    update: added manifest, sitemap and robots

--- app/components/RegisterBtn.jsx
+++ app/components/RegisterBtn.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
 import Image from "next/image"
 
-const RegisterButton = ({ text = "Register now", icon = "/Register-icon.svg", href = "/Register" }) => {
+const RegisterButton = ({ text = "Register now", icon = "/Register-icon.svg", href = "/register" }) => {
   return (
     <Link href={href} className="flex items-center gap-4 px-4 py-2 bg-primary rounded-md transition">
       {/* Render the Text */}
