13055eabc3800e25350c2128a885783aaba54814
Moshood Kausar <69727230+Moshood-Kausar@users.noreply.github.com>
2024-09-29 06:11:50 +0100

    Update About.jsx

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
       <div className="w-full lg:w-1/2">
         <h1 className="font-medium text-center md:text-start text-[18px] lg:text-[28px] mb-1">About Our Hackathon</h1>
 
-        <p className={` text-xs font-thin text-dark md:text-[16px] text-center md:text-start`}>{`Join the RAIN Hackathon 2024, where innovators and trailblazers in Robotics and Artificial Intelligence will come together to solve Africa’s most urgent challenges. This event is a melting pot for the brightest minds, offering a platform to create impactful solutions that shape the future of technology on the continent.`}</p>
+        <p className={` text-xs font-thin text-dark md:text-[16px] text-center md:text-start`}>{`Join the RAIN Hackathon 1.0, where innovators and trailblazers in Robotics and Artificial Intelligence will come together to solve Africa’s most urgent challenges. This event is a melting pot for the brightest minds, offering a platform to create impactful solutions that shape the future of technology on the continent.`}</p>
       </div>
 
       <div className=" lg:w-1/2">
